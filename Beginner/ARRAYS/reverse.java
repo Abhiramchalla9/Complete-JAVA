864717212ef00d93004de485bf7c3e499d240489
Abhiramchalla9 <chs.abhiram@gmail.com>
2023-09-29 22:17:24 +0530

    operators

---
+++ Beginner/ARRAYS/reverse.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class reverse {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the elements in the array :");
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+
+        for(int i=0;i<n;i++){
+             arr[i] = sc.nextInt();
+        }
+        
+
+
+    }
+    
+}
