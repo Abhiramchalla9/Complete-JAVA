fd4a0fa857a6b689b2b33b96f51fa00dbdc6d01c
Abhiramchalla9 <chs.abhiram@gmail.com>
2023-09-29 23:46:12 +0530

    1to100

---
+++ Operators/even1to100.java
@@ -0,0 +1,20 @@
+package Operators;
+import java.util.Scanner;
+public class even1to100 {
+    public static void main(String args[]){
+
+        Scanner sc = new Scanner(System.in);
+
+        //Intializing the sum 
+        int sum=0;
+        for(int i=1;i<=100;i++){
+            if(i%2==0){
+                sum = sum+i;
+            }
+        }
+
+        System.out.println(" The sum of the even numbers from 1 to 100 is :"+sum);
+        
+    }
+    
+}
---
+++ Operators/rough.java
@@ -0,0 +1,9 @@
+package Operators;
+
+public class rough {
+    public static void main(String args[]){
+        int c='C'-'A';
+        System.out.println(c);
+    }
+    
+}
